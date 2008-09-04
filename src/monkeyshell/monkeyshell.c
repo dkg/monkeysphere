d64d1d121a02d8b7d6af6be1b544fdbfcc55d151
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2008-09-04 19:12:09 -0400

    added comments to monkeyshell.c

--- src/monkeyshell/monkeyshell.c
+++ src/monkeyshell/monkeyshell.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/* 
+   Author: Daniel Kahn Gillmor <dkg@fifthhorseman.net> 
+   Date: 2008-09-04 19:11:18-0400
+   License: GPL v3+, if you actually care
+
+   This is a toy "shell" to install on demonstration systems.  If a
+   user can log in successfully, they get this cheery message, and an
+   unusual error code. 
+*/
+
 int main()
 { 
   printf("monkeys!\n");
